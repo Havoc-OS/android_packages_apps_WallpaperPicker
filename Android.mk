cbca9f988f510b6fb40b3f66205ff4708ea381a3
Unknown <anushekprasal@gmail.com>
2018-06-23 02:40:17 +0530

    Add Havoc Wallpapers

--- Android.mk
+++ Android.mk
@@ -24,12 +24,6 @@ LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
-ifeq ($(shell test $(TARGET_SCREEN_WIDTH) -gt 1080; echo $$?),0)
-LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res_1440p
-else
-LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res_1080p
-endif
-
 LOCAL_AAPT_FLAGS := --auto-add-overlay
 
 LOCAL_SDK_VERSION := current
